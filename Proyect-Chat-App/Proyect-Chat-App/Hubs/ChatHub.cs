9364c606ea74f893fda75ca36509d02a1257550e
Nemo3003 <ulimarian2000@gmail.com>
2022-09-02 12:01:53 -0300

    [UPD]Added functionalities

--- Proyect-Chat-App/Proyect-Chat-App/Hubs/ChatHub.cs
+++ Proyect-Chat-App/Proyect-Chat-App/Hubs/ChatHub.cs
@@ -4,6 +4,7 @@ namespace SignalRChat.Hubs
 {
     public class ChatHub : Hub
     {
+        //The SendMessage method can be called by a connected client to send a message to all clients
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
